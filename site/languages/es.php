fc2f8648a9c44a2c754be6e17f5c5739337fe572
Jaume <jaume.ferrete@gmail.com>
2022-12-04 21:14:36 +0100

    config and langs

---
+++ site/languages/es.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'code' => 'es',
+    'default' => false,
+    'direction' => 'ltr',
+    'locale' => [
+        'LC_ALL' => 'es_ES'
+    ],
+    'name' => 'Castellano',
+    'translations' => [
+
+    ],
+    'url' => NULL
+];
